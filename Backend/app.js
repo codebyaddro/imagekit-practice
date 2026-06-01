47a606b5f063c7e8fad0888f8d67409b0b89425a
codebyaddro <codebyaddro@gmail.com>
2026-06-02 00:41:14 +0600

    cookie-parser added

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 const noteRoutes = require("./routes/post.route");
 const userRoutes = require("./routes/user.route");
@@ -8,6 +9,7 @@ const userRoutes = require("./routes/user.route");
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/v1/posts", noteRoutes);
 app.use("/api/v1/users", userRoutes);
